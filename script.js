ac1a596e97b3ee346d18d368cf88f07684346e3d
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-09-19 21:02:22 +0200

    MAKE THE PIXEL GRID
    And the pixel grid was made thanks to a nested loop with Javascript,
    and Flexbox in CSS.
    But the functionality is not made yet.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello, word");
+
+//MAIN PROGRAM
+document.body.onload=main;
+
+function main(){
+
+    createPixels (16, 16);
+    
+}
+
+function createPixels (rows, cols){
+    
+    const container=document.querySelector("#container");
+    for (let i=1; i<=rows; i++){
+        const rowDiv=document.createElement("div");
+        rowDiv.classList.add("rowDiv");
+        for (let j=1;j<=cols; j++){
+            const pixel=document.createElement("span");
+            pixel.classList.add("pixel");
+            rowDiv.appendChild(pixel);
+        }
+
+        container.appendChild(rowDiv);
+    }
+    
+
+}
